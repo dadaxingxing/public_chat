9a6a0eb0bf352e1241dd5a8281e8cb66b78c8cf6
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-01-18 18:58:16 -0800

    added the base and /chat root for server

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,22 @@
+from flask import Flask, request, redirect, session
+from flask_socketio import join_room, leave_room, send, SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'abc'
+socketio = SocketIO(app)
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    pass
+
+@app.route('/chat', methods=['POST', 'GET'])
+def chat():
+    pass
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
+
+
 
