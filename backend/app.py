8f0a585f496de7633d662a5072780c89ff0b7e08
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-01-20 15:36:06 -0800

    setup three API's in the backend

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,51 @@
-from flask import Flask, request, redirect, session
+from flask import Flask, request, session, jsonify
 from flask_socketio import join_room, leave_room, send, SocketIO
+from pymongo import MongoClient
+from uuid import uuid4
+from datetime import datetime
+
+client = MongoClient('mongodb://localhost:27017')
+db = client.public_chat
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abc'
 socketio = SocketIO(app)
 
-@app.route('/', methods=['POST', 'GET'])
-def home():
-    pass
+@app.route('/api/start', methods=['POST'])
+def start_chat():
+    user_id = str(uuid4())
+    user_data = {
+        'usesr_id': user_id
+    }
 
-@app.route('/chat', methods=['POST', 'GET'])
+    db.users.insert_one(user_data)
+    return jsonify({
+        'user_id': user_id,
+        'message': 'Welcome to Blorp Chat!'
+    })
+
+
+@app.route('/api/history', methods=['GET'])
+def history():
+    messages = list(db.messages.find({}, {'_id': 0}))
+    return jsonify({'messages': messages})
+
+
+@app.route('/api/chat', method=['POST'])
 def chat():
-    pass
+    data = request.get_json()
+    userId = data.get('userId')
+    message = data.get('message')
+
+    if not userId or not message:
+        return jsonify({'error': 'User ID and a message is required!'}), 400
+
+    message_data = {
+        'userId': userId,
+        'message': message,
+        'timestamp': datetime.utcnow().isoformat()
+    }
+    db.messages.insert_one(message_data)
 
 
 if __name__ == '__main__':
