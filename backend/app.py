b5f69bac2a942328b32710c210487b71f3e7cad0
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-02-19 23:07:02 -0800

    finished authentication decorator - backend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, session, jsonify
 from flask_socketio import join_room, leave_room, send, SocketIO
 from pymongo import MongoClient
-from uuid import uuid4
 from datetime import datetime
 import jwt
 from dotenv import dotenv_values
+from google.oauth2 import id_token
+from google.auth.transport import requests as google_requests
+from functools import wraps
+import requests
 
 
+google_client_id = dotenv_values('login.env')['CLIENT_ID']
 client = MongoClient('mongodb://localhost:27017')
 db = client.public_chat
 
@@ -18,21 +22,42 @@ socketio = SocketIO(app)
 def auth(func):
     @wraps(func)
     def decorated_function(*args, **kws):
-        pass
+        auth_header = request.headers.get('Authorization')
 
+        if not auth_header:
+            return jsonify({'error': 'Missing Token'}), 400
+
+        token = auth_header.split()[-1]
+        try:
+            response = requests.get(
+                'https://www.googleapis.com/oauth2/v3/userinfo',
+                headers={'Authorization': f'Bearer {token}'}
+            )
+
+            if response.status_code != 200:
+                return jsonify({'error': 'Invalid Token or Token Expired'}), 400
+
+            user_info = response.json()
+            user_email = user_info['email']
+
+            
+        except Exception as e:
+            return jsonify({'error': 'Unknown error occured'}), 400
+
+        return func(user_email, *args, **kws)
     
-    return auth
+    return decorated_function
 
 @app.route('/api/history', methods=['GET'])
 @auth
-def history():
+def history(user):
     messages = list(db.messages.find({}, {'_id': 0}))
     return jsonify({'messages': messages})
 
 
-@app.route('/api/chat', method=['POST'])
+@app.route('/api/chat', methods=['POST'])
 @auth
-def chat():
+def chat(user):
     data = request.get_json()
     userId = data.get('userId')
     message = data.get('message')
@@ -46,7 +71,6 @@ def chat():
         'timestamp': datetime.utcnow().isoformat()
     }
     db.messages.insert_one(message_data)
-
 
 
 if __name__ == '__main__':
