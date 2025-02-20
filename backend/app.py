2aea2dc43b9a836b37ca51be8dd414f74bc50bc2
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-02-19 17:26:49 -0800

    started on authentication decorator - backend

--- backend/app.py
+++ backend/app.py
@@ -14,32 +14,29 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = dotenv_values('login.env')['SECRET_KEY']
 socketio = SocketIO(app)
 
-@app.route('/api/start', methods=['POST'])
-def start_chat():
-    user_id = str(uuid4())
-    user_data = {
-        'usesr_id': user_id
-    }
 
-    db.users.insert_one(user_data)
-    return jsonify({
-        'user_id': user_id,
-        'message': 'Welcome to Blorp Chat!'
-    })
+def auth(func):
+    @wraps(func)
+    def decorated_function(*args, **kws):
+        pass
 
+    
+    return auth
 
 @app.route('/api/history', methods=['GET'])
+@auth
 def history():
     messages = list(db.messages.find({}, {'_id': 0}))
     return jsonify({'messages': messages})
 
 
 @app.route('/api/chat', method=['POST'])
+@auth
 def chat():
     data = request.get_json()
     userId = data.get('userId')
     message = data.get('message')
-
+    
     if not userId or not message:
         return jsonify({'error': 'User ID and a message is required!'}), 400
 
@@ -50,9 +47,6 @@ def chat():
     }
     db.messages.insert_one(message_data)
 
-@app.route('/api/auth', method=['POST'])
-def auth():
-    pass
 
 
 if __name__ == '__main__':
